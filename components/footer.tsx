dc85613b6d957d3c7eb56f978cac48ed9d07b029
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-05-20 14:57:30 +0200

    Add Footer and Header components

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="h-16 flex items-center justify-center bg-gray-100">
+      <div>This is the Footer!</div>
+    </footer>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="h-16 flex items-center justify-center bg-gray-100">
+      <div>This is the Header!</div>
+    </header>
+  );
+}

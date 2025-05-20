6d903a254c3d0ac4270677922704e464ff9ae187
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-05-20 15:34:36 +0200

    Add prettier extension

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="h-16 flex items-center justify-center bg-gray-100">
+    <footer className="flex h-16 items-center justify-center bg-gray-100">
       <div>This is the Footer!</div>
     </footer>
   );

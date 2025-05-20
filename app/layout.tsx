685bb5b09fc11477d5deb044c3c74ac2b08bea91
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-05-20 15:49:11 +0200

    Refactor login page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col antialiased`}
       >
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex flex-1 flex-col">{children}</main>
         <Footer />
       </body>
     </html>

53994023fb5dde5d8ac93d34a9974c4a708749e5
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-05-20 15:18:49 +0200

    Remove dark mode styles and adjust margin for welcome header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="grid place-items-center p-8 sm:p-20 text-center sm:text-left">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+    <div className="p-8 sm:p-20 text-center sm:text-left">
+      <h1 className="text-4xl font-bold m-8">Welcome to Next.js!</h1>
       <p className="text-lg mt-4">
         Get started by editing <code className="font-mono">app/page.tsx</code>
       </p>

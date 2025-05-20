6ac41442967b5d3db4b9dc3272fac08dc3ed4aa5
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-05-20 15:25:45 +0200

    Fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="p-8 sm:p-20 text-center sm:text-left">
+    <div className="place-items-center p-8 sm:p-20 text-center sm:text-left">
       <h1 className="text-4xl font-bold m-8">Welcome to Next.js!</h1>
       <p className="text-lg mt-4">
         Get started by editing <code className="font-mono">app/page.tsx</code>

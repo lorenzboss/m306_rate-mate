a286230dd860637fe1f835d4f8f4cc9983660d5c
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-05-27 13:55:10 +0200

    Update styles in globals.css and footer.tsx for consistency

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex h-16 items-center justify-center bg-gray-100">
+    <footer className="flex h-16 items-center justify-center bg-slate-200">
       <div>This is the Footer!</div>
     </footer>
   );

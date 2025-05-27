589ba23b3c9a48d8b2c5c95c699acf76f0158fae
levin-fankhauser <levin.fankhauser@gmx.ch>
2025-05-27 14:59:32 +0200

    Added header and improved code structure

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,37 @@
+import Link from "next/link";
+
 export default function Header() {
+  const isAdmin = true; // Replace with logic
   return (
-    <header className="flex h-16 items-center justify-center bg-gray-100">
-      <div>This is the Header!</div>
+    <header className="m-4 flex h-16 items-center justify-between rounded-full border-2 border-slate-200 px-5 shadow-md">
+      <div className="flex items-center gap-5">
+        <Link
+          href="/"
+          className="size-18 transition-all duration-300 hover:scale-110"
+        >
+          <img src="icons/home.png" alt="R8M8 Logo" />
+        </Link>
+        <Link
+          href="/review/create"
+          className="font-semibold transition-all duration-300 hover:scale-105"
+        >
+          Create Review
+        </Link>
+        <Link
+          href="/analytics"
+          className={`${!isAdmin && "hidden"} font-semibold transition-all duration-300 hover:scale-105`}
+        >
+          Analytics (Admin only)
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <Link
+          href="/users"
+          className={`${!isAdmin && "hidden"} font-semibold transition-all duration-300 hover:scale-105`}
+        >
+          User Management (Admin only)
+        </Link>
+      </div>
     </header>
   );
 }

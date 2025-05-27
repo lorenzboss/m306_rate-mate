0345ea4d037fdaa441c4e1a49e51862fc0a44ffa
levin-fankhauser <levin.fankhauser@gmx.ch>
2025-05-27 15:54:04 +0200

    Created footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,34 @@
 export default function Footer() {
   return (
-    <footer className="flex h-16 items-center justify-center bg-slate-200">
-      <div>This is the Footer!</div>
+    <footer className="flex h-20 flex-col items-center justify-between bg-slate-200">
+      <div className="h-2 w-full bg-linear-to-t from-slate-200 to-slate-100" />
+      <p className="mb-6 flex w-1/3 flex-row justify-evenly">
+        Authors
+        <a
+          href="https://github.com/lorenzboss"
+          target="_blank"
+          className="font-bold transition-all duration-300 hover:text-violet-500"
+          rel="noreferrer"
+        >
+          Lorenz Boss
+        </a>
+        <a
+          href="https://github.com/levin-fankhauser"
+          target="_blank"
+          className="font-bold transition-all duration-300 hover:text-violet-500"
+          rel="noreferrer"
+        >
+          Levin Fankhauser
+        </a>
+        <a
+          href="https://github.com/BrickiBulli"
+          target="_blank"
+          className="font-bold transition-all duration-300 hover:text-violet-500"
+          rel="noreferrer"
+        >
+          Seth Schmutz
+        </a>
+      </p>
     </footer>
   );
 }

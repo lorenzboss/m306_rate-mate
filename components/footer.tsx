bb58aec26ecea7cd3bfe73b6795f0e9e09719652
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-05-31 23:31:42 +0200

    Refactor footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,33 +1,35 @@
 export default function Footer() {
+  const authors = [
+    {
+      name: "Lorenz Boss",
+      github: "https://github.com/lorenzboss",
+    },
+    {
+      name: "Levin Fankhauser",
+      github: "https://github.com/levin-fankhauser",
+    },
+    {
+      name: "Seth Schmutz",
+      github: "https://github.com/BrickiBulli",
+    },
+  ];
+
   return (
     <footer className="flex h-20 flex-col items-center justify-between bg-slate-200">
       <div className="h-2 w-full bg-linear-to-t from-slate-200 to-slate-100" />
-      <p className="mb-6 flex w-1/3 flex-row justify-evenly">
+      <p className="mb-6 flex w-1/2 flex-row justify-evenly xl:w-1/3">
         Authors
-        <a
-          href="https://github.com/lorenzboss"
-          target="_blank"
-          className="font-bold transition-all duration-300 hover:text-violet-500"
-          rel="noreferrer"
-        >
-          Lorenz Boss
-        </a>
-        <a
-          href="https://github.com/levin-fankhauser"
-          target="_blank"
-          className="font-bold transition-all duration-300 hover:text-violet-500"
-          rel="noreferrer"
-        >
-          Levin Fankhauser
-        </a>
-        <a
-          href="https://github.com/BrickiBulli"
-          target="_blank"
-          className="font-bold transition-all duration-300 hover:text-violet-500"
-          rel="noreferrer"
-        >
-          Seth Schmutz
-        </a>
+        {authors.map((author) => (
+          <a
+            key={author.github}
+            href={author.github}
+            target="_blank"
+            className="font-bold transition-all duration-300 hover:text-violet-500"
+            rel="noreferrer"
+          >
+            {author.name}
+          </a>
+        ))}
       </p>
     </footer>
   );

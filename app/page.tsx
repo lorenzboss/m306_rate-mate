22211bf3e04d06af3c2b75d89299e43e963d89cf
levin-fankhauser <levin.fankhauser@gmx.ch>
2025-06-17 14:56:37 +0200

    Refactor Home component to include session validation and user login prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import UserReviews from "@/components/user-reviews";
+import { requireSession } from "@/lib/session";
+import { SquareArrowUpRight } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const session = await requireSession();
+  if (!session?.user?.id) {
+    return (
+      <div className="flex h-[45vh] items-center justify-center gap-5">
+        <div className="flex text-4xl font-bold">
+          Please log in to see your reviews
+        </div>
+        <SquareArrowUpRight className="size-10" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex h-[80vh] items-center justify-center gap-10">
       <UserReviews type="created" />
